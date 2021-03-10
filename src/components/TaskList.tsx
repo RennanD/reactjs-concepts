cfb60d423cac7806db601b3e373ee8b3538cdea8
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-10 14:49:40 -0300

    feat: create app

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,35 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(newTaskTitle) {
+      const newTask = {
+        id: Math.floor(Math.random() * 10) + 1,
+        title: newTaskTitle,
+        isComplete: false
+      }
+  
+      setTasks(state => [...state, newTask])
+    }
+    
+    setNewTaskTitle('')
+
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(state => state.map(task => {
+      if(task.id === id) {
+        return {
+          ...task,
+          isComplete: true
+        }
+      }
+      return task
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(state => state.filter(task => task.id !== id))
   }
 
   return (
